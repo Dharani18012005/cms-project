52432f86a5b11954733de7f980f74d2cb79d850c
Dharani <dharuu1801@gmail.com>
2025-05-29 15:11:23 +0530

    refactor: remove unused imports and clean up code structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 // import Signup from './pages/Signup'
 import Developerspage from './pages/Developerspage'
 import './App.css'
-// import BasicLayouts from './layouts/BasicLayouts'
-import DashboardLayout from './layouts/DashboardLayout'
  
 function App() {
 
