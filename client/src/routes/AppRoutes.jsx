724e62fe3adcb04c1291afc3d54e4f0633b57bc9
Dharani <dharuu1801@gmail.com>
2025-05-31 15:47:39 +0530

    updated

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,18 +1,18 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import SignUp from "../pages/Signup";
-// import About from "../pages/About";
+import About from "../pages/About";
 import BasicLayouts from "../layouts/BasicLayouts";
 import DashboardLayout from "../layouts/DashboardLayout";
 import Intro from "../pages/dashboard/Intro";
-import About from "../pages/dashboard/About";
 import Developerspage from "../pages/dashboard/Developerspage";
 import Tours from "../pages/dashboard/Tours";
 import Guides from "../pages/dashboard/Guides";
 import Bookings from "../pages/dashboard/Bookings";
 import Reviews from "../pages/dashboard/Reviews";
 import PrivateRoute from "./PrivateRoute";
+import { useNavigate } from "react-router-dom";
 
 export default function AppRoutes() {
     const navigate = useNavigate();
@@ -20,11 +20,11 @@ export default function AppRoutes() {
         <Routes>
             <Route path="/" element={<BasicLayouts />}>
                 <Route index element={<Home />} />
-                <Route path="home" element={<Home />} />
-                <Route path="signin" element={<Login />} />
-                <Route path="login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/signin" element={<Login />} />
+                <Route path="/login" element={<Login />} />
                 <Route
-                    path="signup"
+                    path="/signup"
                     element={
                         <SignUp
                             onSignUpSuccess={() => {
@@ -33,25 +33,24 @@ export default function AppRoutes() {
                         />
                     }
                 />
-                <Route path="about" element={<About />} />
-                <Route path="developers" element={<Developerspage />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/developers" element={<Developerspage />} />
             </Route>
-            <Route
-                path="/dashboard"
-                element={
-                    <PrivateRoute>
-                        <DashboardLayout />
-                    </PrivateRoute>
-                }
-            >
-                <Route path="intro" element={<Intro />} />
-                <Route path="about" element={<About/>}/>
-                <Route path="developer" element={<Developerspage />} />
-                <Route path="Tours" element={<Tours />} />
-                <Route path="Guides" element={<Guides />} />
-                <Route path="Bookings" element={<Bookings />} />
-                <Route path="Reviews" element={<Reviews />} />
-            </Route>
-        </Routes>
-    );
+        <Route
+            path="/dashboard"
+            element={
+                <PrivateRoute>
+                    <DashboardLayout />
+                </PrivateRoute>
+            }
+        >
+            <Route path="/dashboard/intro" element={<Intro />} />
+            <Route path="/dashboard/Developerspage" element={<Developerspage/>}/>
+            <Route path="/dashboard/Tours" element={<Tours />} />
+            <Route path="/dashboard/Guides" element={<Guides />} />
+            <Route path="/dashboard/Bookings" element={<Bookings />} />
+            <Route path="/dashboard/Reviews" element={<Reviews />} />
+        </Route>
+    </Routes>
+);
 }
